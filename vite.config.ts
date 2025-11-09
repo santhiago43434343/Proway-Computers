ec998514c4783ad14efe6fb03e3914c850938e93
santhiago <santhiago34343434@gmail.com>
2025-11-09 00:23:07 -0300

    atualização para vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,18 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
-import angular from '@analogjs/vite-plugin-angular';
-import history from 'connect-history-api-fallback';
-import path from 'path'; // ⬅️ Adicione este import
 
-export default defineConfig({
-  base: "/PrivacyComputer/", /*PARA URL GITHUB PAGES */
-  plugins: [angular()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@services': path.resolve(__dirname, 'src/app/services'),
-      '@components': path.resolve(__dirname, 'src/app/components')
-    }
-  },
-  server: {
-    host: true,
-    fs: {
-      strict: false
+export default defineConfig(({ mode }) => {
+  return {
+    // Alterna automaticamente entre localhost e GitHub Pages
+    base: mode === 'production' ? '/Proway-Computers/' : '/',
+    
+    build: {
+      outDir: 'docs' // GitHub Pages espera a pasta docs/
     },
-    middlewareMode: false,
-    //@ts-expect-error: Vite não reconhece setupMiddlewares na tipagem
-    setupMiddlewares: (middlewares) => {
-      middlewares.use(history({
-        index: '/index.html',
-        verbose: true,
-        disableDotRule: true,
-        htmlAcceptHeaders: ['text/html'],
-        rewrites: [
-          { from: /^\/api\/.*$/, to: '/index.html'}
-        ]
-      }));
-      return middlewares;
+
+    server: {
+      port: 4200,              // Porta igual ao Angular CLI
+      historyApiFallback: true // Evita 404 em rotas Angular
     }
-  },
-  build: {
-    outDir: 'dist/ecommerce',
-    target: 'es2022'
-  }
+  };
 });
-
-
- 
